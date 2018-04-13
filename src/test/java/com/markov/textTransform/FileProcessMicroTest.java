cbe2d81f64bd46a68e889ce8ac25710560ea9337
Reza.sh <Reza.sh@192.168.2.33>
2018-04-13 00:23:30 -0400

    adding pom files for microtest

---
+++ src/test/java/com/markov/textTransform/FileProcessMicroTest.java
@@ -0,0 +1,9 @@
+package com.markov.textTransform;
+
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FileProcessMicroTest {
+
+}
